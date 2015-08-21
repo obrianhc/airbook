ad616ff915b3bbf74643d6fe76d7eb93b909ff18
luisfya <luisfyaplus@gmail.com>
2015-08-21 15:05:18 -0600

    Uso de select
    Select para filtrar por categoria, falta insersión de solicitud

--- solicitudes/Llamar_solicitud.php
+++ solicitudes/Llamar_solicitud.php
@@ -4,33 +4,109 @@
 	*/
 	class Llamar_solicitud
 	{
-		
-		function get_all_solicitud()
-		{
-			require_once("db.php");
-			$link = mysqli_connect($server, $user, $pass, $db) or die("Error " . mysqli_error($link)); 
-			$query = "SELECT usuario.nombre as nom_usuario, categoria.nombre as nom_categoria, solicitud.comentario as comentario, solicitud.fecha as fecha 
-					  FROM solicitud, categoria, usuario 
-					  WHERE solicitud.id_usuario = usuario.id_usuario AND solicitud.id_categoria = categoria.id_categoria 
-					  ORDER BY solicitud.fecha DESC" 
-			or die("Error en la consulta.." . mysqli_error($link));
-			$resultado = $link->query($query); 
-
-			return $resultado;
+		function get_categorias(){
+			require("db.php");
+			$query = "SELECT * FROM categoria";
+			$result = mysqli_query($connect, $query);
+			$lista = array();
+			while($row = mysqli_fetch_array($result)){
+				$elemento = new Categoria($row[0], $row[1]);
+				$lista[] = $elemento;
+			}
+			mysqli_close($connect);
+			return $lista;
 		}
 
-		function get_filtro_solicitud($categoria)
-		{
-			require_once("db.php");
-			$link = mysqli_connect($server, $user, $pass, $db) or die("Error " . mysqli_error($link)); 
-			$query = "SELECT usuario.nombre as nom_usuario, categoria.nombre as nom_categoria, solicitud.comentario as comentario, solicitud.fecha as fecha 
-					  FROM solicitud, categoria, usuario 
-					  WHERE solicitud.id_usuario = usuario.id_usuario AND solicitud.id_categoria = categoria.id_categoria AND categoria.nombre = '$categoria'
-					  ORDER BY solicitud.fecha DESC" 
-			or die("Error en la consulta.." . mysqli_error($link));
-			$resultado = $link->query($query); 
+		function get_solicitudes_todas(){
+			require("db.php");
+			$query = "SELECT solicitud.id_solicitud, usuario.nombre as nom_usuario, categoria.nombre as nom_categoria, solicitud.comentario as comentario, solicitud.fecha as fecha 
+					  FROM usuario, categoria, solicitud 
+					  WHERE usuario.id_usuario = solicitud.id_usuario AND categoria.id_categoria = solicitud.id_categoria";
+			$result = mysqli_query($connect, $query);
+			$lista = array();
+			while($row = mysqli_fetch_array($result)){
+				$elemento = new Solicitud($row[0], $row[1], $row[2], $row[3], $row[4]);
+				$lista[] = $elemento;
+			}
+			mysqli_close($connect);
+			return $lista;
 
-			return $resultado;
+		}
+
+		function get_solicitudes_todas_filtrada($id_categoria){
+			require("db.php");
+			$str_filtro = "";
+			if($id_categoria > 0)
+				$str_filtro = $str_filtro . "AND solicitud.id_categoria = $id_categoria";
+			$query = "SELECT solicitud.id_solicitud, usuario.nombre as nom_usuario, categoria.nombre as nom_categoria, solicitud.comentario as comentario, solicitud.fecha as fecha 
+					  FROM usuario, categoria, solicitud 
+					  WHERE usuario.id_usuario = solicitud.id_usuario AND categoria.id_categoria = solicitud.id_categoria $str_filtro";
+			$result = mysqli_query($connect, $query);
+			$lista = array();
+			while($row = mysqli_fetch_array($result)){
+				$elemento = new Solicitud($row[0], $row[1], $row[2], $row[3], $row[4]);
+				$lista[] = $elemento;
+			}
+			mysqli_close($connect);
+			return $lista;
+
+		}
+	}
+
+	class Solicitud{
+		var $id;
+		var $nom_usuario;
+		var $nom_categoria;
+		var $comentario;
+		var $fecha;
+
+		public function __construct($id, $nom_usuario, $nom_categoria, $comentario, $fecha){
+			$this->id = $id;
+			$this->nom_usuario = $nom_usuario;
+			$this->nom_categoria = $nom_categoria;
+			$this->comentario = $comentario;
+			$this->fecha = $fecha;
+		}
+
+		function getId(){
+			return $this->id;
+		}
+
+		function getNomUsuario(){
+			return $this->nom_usuario;
+		}
+
+		function getNomCategoria(){
+			return $this->nom_categoria;
+		}
+		function getComentario(){
+			return $this->comentario;
+		}
+		function getFecha(){
+			return $this->fecha;
+		}
+	}
+
+	class Categoria{
+		var $id;
+		var $nombre;
+
+		public function __construct($id, $nombre){
+			$this->id = $id;
+			$this->nombre = $nombre;
+		}
+
+		function setId($id){
+			$this->id = $id;
+		}
+		function getId(){
+			return $this->id;
+		}
+		function setNombre($nombre){
+			$this->nombre = $nombre;
+		}
+		function getNombre(){
+			return $this->nombre;
 		}
 	}
 ?>
--- solicitudes/solicitudes.php
+++ solicitudes/solicitudes.php
@@ -4,16 +4,29 @@
 	<title>Solicitudes de Contenido a Subir</title>
 </head>
 <body>
+	<?php 
+		require_once('Llamar_Solicitud.php');
+		$solicitud = new Llamar_Solicitud();
+	?>
 	<h2>Solicitudes</h2>
 	<h3>¿Qué es lo que buscas?</h3>
 	<form method="POST" enctype="multipart/form-data">
 		<input type="text" name="txt_categoria" size="15">
+		<select name="opt_categorias">
+			<option value = 0>--Categorias--</option>
+			<?php 
+				$lis_cat = $solicitud->get_categorias();
+				$x = 0;
+				while($x < count($lis_cat)) { 
+					echo "<option value = ".$lis_cat[$x]->getId().">";
+					echo $lis_cat[$x]->getNombre();
+					echo "</option>"; 
+					$x++;
+				} 
+			?>
+		</select>
 		<input type="submit" value = "Filtrar por categoria" name="btn_por_categoria"/>
 		<table id="tbl_solicitudes">
-			<?php 
-				require_once('Llamar_Solicitud.php');
-				$solicitud = new Llamar_Solicitud();
-			?>
 			<thead>
 				<th>Usuario</th>
 				<th>Categoria</th>
@@ -22,27 +35,25 @@
 			</thead>
 			<tbody>
 					<?php 
-						require_once('Llamar_Solicitud.php');
-						if(isset($_POST['btn_por_categoria'])){
-							$valor_categoria = $_POST['txt_categoria'];
-							if($valor_categoria == ''){
-								$resultado = $solicitud->get_all_solicitud();
-							}else{
-								$resultado = $solicitud->get_filtro_solicitud($valor_categoria);
-							}
-						}else {
-							$resultado = $solicitud->get_all_solicitud();
-						}
-
-						while($col = mysqli_fetch_array($resultado)) { 
-							echo "<tr>";
-							echo "<td>" . $col["nom_usuario"] . "</td>"; 
-							echo "<td>" . $col["nom_categoria"] . "</td>"; 
-							echo "<td>" . $col["comentario"] . "</td>"; 
-							echo "<td>" . $col["fecha"] . "</td>"; 
-							echo "</tr>";
-						} 
-					?>	
+					if(isset($_POST['btn_por_categoria'])){
+						$sel_id_categoria = $_POST['opt_categorias']; 
+						$lis_sol = $solicitud->get_solicitudes_todas_filtrada($sel_id_categoria);
+						
+					}else{
+						$lis_sol = $solicitud->get_solicitudes_todas();	
+					}
+					$x = 0;
+					while($x < count($lis_sol)) {
+						echo "<tr>";
+						echo "<td>".$lis_sol[$x]->getNomUsuario()."</td>"; 
+						echo "<td>".$lis_sol[$x]->getNomCategoria()."</td>"; 
+						echo "<td>".$lis_sol[$x]->getComentario()."</td>"; 
+						echo "<td>".$lis_sol[$x]->getFecha()."</td>"; 
+						echo "</tr>";
+						$x++;
+					}
+						
+					?>
 			</tbody>
 		</table>
 	</form>
