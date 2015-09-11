7218c5b6dcaf01afc3066499465c390c926c9bee
obrianhc <obrian.hc@gmail.com>
2015-09-11 11:56:15 -0600

    Ranking
    No se habia tomado en cuenta que se necesitaba información adicional en
    los métodos de la clase, esta información es:
    
    $ranking->rate(id_usuario, id_libro, puntuacion);
    
    Se ha corrido la prueba y se obtienen los mismos errores que
    visualizamos anteriormente.

--- ranking/testRanking.php
+++ ranking/testRanking.php
@@ -10,15 +10,15 @@
 
 		public function testRate(){
 			$ranking = new Ranking();
-			$rate = $ranking->rate(3); // Valores para calificar de 1-5, se espera que responda si el valor es correcto
+			$rate = $ranking->rate(1,1,3); // Valores para calificar de 1-5, se espera que responda si el valor es correcto. id_usuario, id_archivo
 			$this->assertTrue($rate);
 		}
 
 		public function testRateFalse(){
 			$ranking = new Ranking();
-			$rate = $ranking->rate(0);
+			$rate = $ranking->rate(1,1,0);
 			$this->AssertFalse($rate);
-			$rate = $ranking->rate(6);
+			$rate = $ranking->rate(1,1,6);
 			$this->assertFalse($rate);
 		}
 
