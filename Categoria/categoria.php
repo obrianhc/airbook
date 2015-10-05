9f20e3c75b79a919e41d1ecbd7dd3759181a7576
obrianhc <obrian.hc@gmail.com>
2015-10-05 13:41:03 -0600

    Documentacion
    Categoria y compartir

---
+++ Categoria/categoria.php
@@ -0,0 +1,20 @@
+<?php
+
+class Categoria{
+
+	//cliente crear categoria
+
+
+
+	//cliente modificar categoria
+
+
+
+	//cliente/administrador eliminar categoria
+
+
+	//aceptar categoria =  en la base de datos se crea un campo con valor 0 o 1 donde cero es para categoria no acetada
+	// despues de aceptar la categoria este valor debe cambiarse a 1 
+}
+
+?>
