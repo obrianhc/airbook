b819a36b762c46e2200d21a9204bd6bcea716bbd
tonyusac <lamevillcor13@gmail.com>
2015-10-07 16:57:58 -0600

    revision 3 de testInbox
    Se modifico el parametro de la funcion assertNotNull y su respectivo
    comentario

--- Inbox/testInbox.php
+++ Inbox/testInbox.php
@@ -9,7 +9,7 @@
 	public function testSendMessageTrue(){
             $inbox = new Inbox();
 			$mensaje = $inbox->setMessage(1,"cualquier texto",2);//se envia el id del remitente, el mensaje y el id del destinatario
-			$this->assertTrue(($mensaje!=null));   //si el resultado es true, la prueba es satisfactoria
+			$this->assertTrue($mensaje);   //si el resultado es true, la prueba es satisfactoria
 
   		
 		}
@@ -17,7 +17,7 @@
         public function testReadMessageTrue(){
             $inbox = new Inbox();
 			$mensaje = $inbox->getMessage(2); // se envia el id del destinatario(usuario actual de sesion que quiere leer sus mensajes) y se obtienen los mensajes.
-			$this->assertNotNull($mensaje);   //si el resultado es true, la prueba es satisfactoria
+			$this->assertNotNull($mensaje);   //si el resultado no es nulo, la prueba es satisfactoria
 
   		
 		}
